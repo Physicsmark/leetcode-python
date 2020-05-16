ffe0858825f7f2e5a525f18bcfeaea62194dee9a
Physicsmark <gmmark12@gmail.com>
2020-05-17 00:34:57 +0800

    0007: Reverse Integer

---
+++ 0007-ReverseInteger.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x >= 0:
+            return int(str(x)[::-1])
+        else:
+            return -int(str(-x)[::-1])
